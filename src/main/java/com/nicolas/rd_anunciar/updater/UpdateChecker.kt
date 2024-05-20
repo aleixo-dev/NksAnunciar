b136b4edb782773bab5c978325047cd69beb3e54
Nicolas Aleixo <75820713+aleixo-dev@users.noreply.github.com>
2024-05-20 10:39:53 -0300

    [update checker] implement notify new version available (#4)
    * bump version
    
    * feat: create notify plugin update
    
    * feat: create update checker

---
+++ src/main/java/com/nicolas/rd_anunciar/updater/UpdateChecker.kt
@@ -0,0 +1,32 @@
+package com.nicolas.rd_anunciar.updater
+
+import com.nicolas.rd_anunciar.Main
+import org.bukkit.Bukkit
+import java.io.IOException
+import java.net.URL
+import java.util.Scanner
+import java.util.function.Consumer
+
+class UpdateChecker(
+    private val plugin: Main,
+    private val resourceId: Int
+) {
+
+    fun getVersion(consumer: Consumer<String>) {
+        Bukkit.getScheduler().runTaskAsynchronously(plugin) {
+            try {
+                val inputStream = URL("$BASE_URL$resourceId/~").openStream()
+                val scanner = Scanner(inputStream)
+                if (scanner.hasNext()) {
+                    consumer.accept(scanner.next())
+                }
+            } catch (exception: IOException) {
+                plugin.logger.info("Não foi possível buscar por atualizações: ${exception.message}")
+            }
+        }
+    }
+
+    companion object {
+        const val BASE_URL = "https://api.spigotmc.org/legacy/update.php?resource="
+    }
+}
